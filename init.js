91a2740966bad7af9734eb5437c757ce4f0ee436
DesignSpacePerson <91597982+DesignSpacePerson@users.noreply.github.com>
2021-10-05 03:58:22 -0400

    Add files via upload

--- init.js
+++ init.js
@@ -27,3 +27,41 @@ function topFunction() {
      var instances = M.Sidenav.init(elems, options);
    });
     });
+
+
+
+
+// for Menu
+
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+
+// $(document).ready(function () {
+//   $('.sidenav').sidenav();
+//   $(".button-collapse").sideNav();
+// });
+
+
+// Input Field Initializer
+$(document).ready(function() {
+  $('input#input_text, textarea#textarea2').characterCounter();
+});
+
+
+// Parallax Initializer
+$(document).ready(function(){
+  $('.parallax').parallax();
+});
+
+
+// Modal Initializer
+$(document).ready(function(){
+    $('.modal').modal();
+  });
